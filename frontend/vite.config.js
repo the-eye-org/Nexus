f240ba4ccf4d50761c2ce82b7e433d1ec45f45e9
Pravith1 <pravith1489181@gmail.com>
2026-02-12 18:24:37 +0530

    changed base route of frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/nexus/', // App expects to run at /nexus path
+  base: '/', // Main nginx strips /nexus before forwarding
   plugins: [react()],
 })
