55a2f2c0a243ce40192a49ea465202851b1ffe12
Pravith1 <pravith1489181@gmail.com>
2026-02-12 19:58:43 +0530

    final try

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/nexus', // App runs at /nexus path
+  base: '/nexus/', // App runs at /nexus path (trailing slash is important)
   plugins: [react()],
 })
